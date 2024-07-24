6ae9dc646a09a09a3dcc616ac3a8471a3993c041
jonsimonsen <j_simon10@hotmail.com>
2024-07-24 20:08:46 +0200

    Add first version of hand printing function

--- dealhand.py
+++ dealhand.py
@@ -1,10 +1,26 @@
 import random
 
-def dealhand():
+def dealHand():
     deck = list(range(52))
     hand = random.sample(deck, 13)
     hand.sort()
     return hand
 
+def printHand(hand):
+    i = 0
+    suit = 1
+    cards = ""
+
+    while(suit < 5):
+        cards += "[ "
+        while(i < 13 and hand[i] < suit * 13 ):
+            cards += str(hand[i]) + " "
+            i += 1
+        suit += 1
+        cards += "]"
+
+    print(cards)
+
+
 # Use
-print(dealhand())
+printHand(dealHand())
